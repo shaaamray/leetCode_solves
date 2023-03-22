07596040e81f7d23fd81143252d74380ad639641
shaaamray <83807318+shaaamray@users.noreply.github.com>
2023-03-23 00:07:01 +0600

    Update Palindrome_Number_9.py

--- Palindrome_Number_9.py
+++ Palindrome_Number_9.py
@@ -2,16 +2,19 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
+            
         div = 1
-        while x >= 10 * div:
+        while x >= 10* div:
             div *= 10
 
         while x:
             right = x % 10
             left = x // div
-
+            
             if right != left:
                 return False
-            x = (x % div) // 10
+            x = x % div # remove left digit
+            x = x // 10 # remove right digit
             div = div / 100
         return True
+            
